1a0d5c8287cd2928f7cd036683ba7e42be886c05
Alexandre Sage (ASE) <alexandre.sage@adsoftware.fr>
2025-07-24 17:17:36 +0200

    refactor(mini-miner): use hackattick api

--- mini-miner/src/main.rs
+++ mini-miner/src/main.rs
@@ -1,10 +1,6 @@
-use anyhow::Context;
 use serde::{Deserialize, Deserializer, Serialize};
 use serde_json::json;
 use sha256::digest;
-
-const PROBLEM_URL: &str = "https://hackattic.com/challenges/mini_miner/problem?access_token=";
-const SOLVE_URL: &str = "https://hackattic.com/challenges/mini_miner/solve?access_token=";
 
 fn parse_nonce<'de, D>(d: D) -> Result<usize, D::Error>
 where
@@ -38,37 +34,22 @@ impl ToString for Block {
     }
 }
 
-async fn challenge() -> anyhow::Result<Challenge> {
-    let token = dotenvy::var("AUTH_TOKEN").unwrap();
-    let url = format!("{}{}", PROBLEM_URL, token);
-
-    let res = reqwest::get(url).await?;
-    res.json().await.with_context(|| "Could not parse json")
-}
-
-async fn submit(response: usize) {
-    let token = dotenvy::var("AUTH_TOKEN").unwrap();
-    let url = format!("{}{}", SOLVE_URL, token);
-
-    let res = reqwest::Client::new()
-        .post(url)
-        .json(&json!({"nonce":response}))
-        .send()
-        .await
-        .unwrap();
-    dbg!(res.json::<serde_json::Value>().await.unwrap());
-}
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let challenge = challenge().await?;
+    dotenvy::dotenv().unwrap();
+    let api = helpers::HackatticApi::new("mini_miner");
+
+    let challenge = api.problem::<Challenge>().await?;
+
     let bytes_needed = challenge.difficulty / 4;
     let mut block = challenge.block;
     let start = (0..bytes_needed).map(|_| 0.to_string()).collect::<String>();
+
     loop {
         let result = digest(block.to_string());
         if result.starts_with(&start) {
-            submit(block.nonce).await;
+            let res = api.solve(&json!({"nonce":block.nonce})).await?;
+            dbg!(res);
             break;
         }
         block.increment_nonce();
